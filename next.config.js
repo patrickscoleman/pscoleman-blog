88bb33796d3798ede7992c01ce3540896dfdf4ca
Patrick Coleman <patrickscoleman@gmail.com>
2022-12-22 17:01:52 -0500

    Start next.js SEO tutorial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./utils/generate-sitemap");
+    }
+
+    return config;
+  },
   async redirects() {
     return [
       {
