0a22d614039f42fafee9d6dafa158700c6ad7de8
Patrick Coleman <patrickscoleman@gmail.com>
2022-12-23 11:48:14 -0500

    Fix bugs with sitemap
    Add back 404.js since it wasn't causing the problem with getting the
    list of ids from /posts.
    
    Instead of hitting the public site to get the posts data, just call the
    getAllPostIds helper function that we'd already written.

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,4 @@
-const EXTERNAL_DATA_URL = `${process.env.WEBSITE_URL}/posts`;
+import { getAllPostIds } from "../utils/posts";
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
@@ -8,10 +8,10 @@ function generateSiteMap(posts) {
        <loc>https://pscoleman.me</loc>
      </url>
      ${posts
-       .map(({ id }) => {
+       .map(({ params }) => {
          return `
        <url>
-           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+           <loc>${`${process.env.WEBSITE_URL}/posts/${params.id}`}</loc>
        </url>
      `;
        })
@@ -25,9 +25,7 @@ function SiteMap() {
 }
 
 export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
+  const posts = await getAllPostIds();
 
   // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(posts);
