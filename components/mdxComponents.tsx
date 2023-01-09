86c631c73fd401fb8246fb53cda3aa32e4438208
Patrick Coleman <patrickscoleman@gmail.com>
2023-01-09 14:34:39 -0800

    add support for custom mdx elements

---
+++ components/mdxComponents.tsx
@@ -0,0 +1,7 @@
+export const MdxComponents = {
+  h1: (props) => (
+    <h1>
+      <em {...props}></em>
+    </h1>
+  ),
+};
--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const withMDX = mdx({
   options: {
     remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     rehypePlugins: [rehypeHighlight],
+    providerImportSource: "@mdx-js/react",
   },
 });
 
