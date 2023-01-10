f640360ba3a750f50fc4bb865dcf5464367d9cd7
Patrick Coleman <patrickscoleman@gmail.com>
2023-01-09 18:28:09 -0800

    install material UI and tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { PostList } from "@/components/postList";
 import utilStyles from "@/styles/utils.module.css";
 import { PostData, getSortedPostsData } from "@/utils/getPosts";
 import { GetStaticProps } from "next";
+import Button from "@mui/material/Button";
+import Link from "next/link";
 
 const Home = ({ allPostsData }: { allPostsData: PostData[] }) => {
   return (
@@ -13,6 +15,11 @@ const Home = ({ allPostsData }: { allPostsData: PostData[] }) => {
         <p>Hi, I'm Patrick. I've just started this blog.</p>
       </section>
       <PostList allPostsData={allPostsData} />
+      <Link href="https://zombo.com" passHref rel="noopener" target="_blank">
+        <Button variant="contained" color="primary">
+          Click me!
+        </Button>
+      </Link>
     </Layout>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    // For the best performance and to avoid false positives,
-    // be as specific as possible with your content configuration
-  ],
+  content: ["./pages/**/*.{tsx, mdx}", "./components/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
