345fc5b2f441d7533877aae5841f1ae11bf200d6
Patrick Coleman <patrickscoleman@gmail.com>
2023-01-27 18:57:30 -0700

    add footer placeholder

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+const FooterComponent = () => {
+  return (
+    <footer className="mt-8 mb-4">
+      <hr />
+      <div className="flex w-full items-center justify-between mt-1">
+        <p>Footer goes here</p>
+      </div>
+    </footer>
+  );
+};
+
+export const Footer = FooterComponent;
