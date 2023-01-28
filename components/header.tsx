972026348508402b11c1a8e84b0ecf30895687da
Patrick Coleman <patrickscoleman@gmail.com>
2023-01-27 17:48:14 -0700

    rearrange header bar

--- components/header.tsx
+++ components/header.tsx
@@ -1,45 +1,47 @@
 import { Menu } from "@/components/menu";
+import { Search } from "@mui/icons-material";
+import { IconButton, Tooltip } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 
+const Title = () => (
+  <div>
+    <h2 aria-label="pscoleman.me/" className="flex text-lg mx-1 my-0">
+      pscoleman
+      <Image
+        priority
+        src="/images/patrick.png"
+        className="rounded-full self-end mb-1.5"
+        height={7}
+        width={7}
+        alt="dot"
+      />
+      me/
+    </h2>
+  </div>
+);
+
 const HeaderComponent = ({ home }: { home: boolean }) => {
   return (
-    <header className="mt-4 mb-8 flex items-center justify-between">
-      <div className="flex items-center">
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/patrick.png"
-              className="rounded-full mx-1"
-              height={40}
-              width={40}
-              alt="Patrick Coleman"
-            />
-            <h2 className="text-lg mx-1 my-0">pscoleman.me/</h2>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/patrick.png"
-                className="rounded-full mx-1"
-                height={40}
-                width={40}
-                alt="Patrick Coleman"
-              />
+    <header className="mt-4 mb-8">
+      <div className="flex w-full items-center justify-between">
+        <>
+          <Menu />
+          {home ? (
+            <Title />
+          ) : (
+            <Link href="/" className="no-underline">
+              <Title />
             </Link>
-            <h2 className="text-lg mx-1 my-0">
-              <Link href="/" className="no-underline">
-                pscoleman.me/
-              </Link>
-            </h2>
-          </>
-        )}
-      </div>
-      <div className="flex items-center">
-        <Menu />
+          )}
+          <Tooltip title="Search coming soon">
+            <span>
+              <IconButton disabled>
+                <Search />
+              </IconButton>
+            </span>
+          </Tooltip>
+        </>
       </div>
     </header>
   );
