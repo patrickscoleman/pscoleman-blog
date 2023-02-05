b0b4340d6ad2b31bf9707861b3d92eabf5ad5ae2
Patrick Coleman <patrickscoleman@gmail.com>
2023-02-05 15:24:40 -0800

    add new file to get adjacent posts

---
+++ utils/getAdjacentPosts.ts
@@ -0,0 +1,28 @@
+import postsList from "../data/postsList.json";
+
+export interface PostData {
+  id: string;
+  date: string;
+  title: string;
+  description?: string;
+}
+
+type Path = `/${string}`;
+
+export const getPrevPost = (id: string): Path | null => {
+  const index = postsList.findIndex((post) => post.id === id);
+  if (index !== -1 && index !== 0) {
+    return `/${postsList[index - 1].id}`;
+  } else {
+    return null;
+  }
+};
+
+export const getNextPost = (id: string): Path | null => {
+  const index = postsList.findIndex((post) => post.id === id);
+  if (index !== -1 && index !== postsList.length - 1) {
+    return `/${postsList[index + 1].id}`;
+  } else {
+    return null;
+  }
+};
