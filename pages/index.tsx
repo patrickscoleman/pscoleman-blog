44c0a18d2a3ef3722b1e492259b172dc486d2012
Patrick Coleman <patrickscoleman@gmail.com>
2023-02-07 20:24:23 -0800

    add blurb to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,21 @@ const Home = () => {
         alt="Patrick Coleman"
       />
       <section className="">
-        <p>Hi, I&apos;m Patrick. I&apos;ve just started this blog.</p>
+        <p>
+          👋 Hello internet friend, I&apos;m Patrick. This is my blog, and in
+          it, you&apos;ll find thoughts on things.
+        </p>
+        <p>
+          It&apos;s too early to tell for sure, but I&apos;ll most likely write
+          about startups, maybe translate some haikus, share dog pics, keep a
+          travelogue, and just jot errant thoughts down as they come up.
+        </p>
+        <p>
+          And so you know that I&apos;m well qualified to share only the highest
+          quality thoughts, please head on over to my about page to see what
+          I&apos;ve been up to on my short time on this space rock that we call
+          Earth.
+        </p>
       </section>
       <PostsList />
     </Layout>
