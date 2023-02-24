c693b8da7b00c495f6530738e1cf7de1f22ad5da
Patrick Coleman <patrickscoleman@gmail.com>
2023-02-24 14:39:04 -0800

    update learning to code draft and 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,14 +5,27 @@ const Custom404 = () => {
   return (
     <Layout>
       <h1 className="text-center">404 - PAGE NOT FOUND - 404</h1>
-      <Image
-        src="/images/frazzled_office_worker.png"
-        className="mx-auto"
-        height={500}
-        width={500}
-        alt="Frazzled Office Worker"
-      />
+      <figure className="w-full text-center">
+        <Image
+          src="/images/frazzled_office_worker.png"
+          className="mx-auto"
+          height={500}
+          width={500}
+          alt="Frazzled Office Worker"
+        />
+        <figcaption className="text-sm text-text-lightfaint dark:text-text-darkfaint">
+          Source:{" "}
+          <a
+            href="https://midjourney.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Midjourney
+          </a>
+        </figcaption>
+      </figure>
       <p className="text-center">We seem to have misplaced your page...</p>
+      <p className="text-center">Sorry.</p>
     </Layout>
   );
 };
