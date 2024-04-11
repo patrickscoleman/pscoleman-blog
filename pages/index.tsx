1a43e85464fdceb8279e50a1f2c6d389e538dd24
Patrick Coleman <patrickscoleman@gmail.com>
2024-04-11 14:37:12 -0700

    fix external link icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,35 +27,30 @@ const Home = () => {
           >
             code a little
           </Link>
-          <OpenInNew className="ml-1" sx={{ fontSize: "1rem" }} /> too.
+          <OpenInNew className="ml-1 text-sm" /> too.
         </p>
         <p>
           Until recently I was traveling around the world on a long honeymoon.
           Before that I co-founded <Link href="/posts/yaya">Yaya</Link>, an AI
           foreign language learning app. I was also the first business hire and
           an early leader at{" "}
-          <span className="flex-inline items-center space-x-1">
-            <Link
-              href="https://replit.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Replit
-            </Link>
-            <OpenInNew className="text-sm" />
-          </span>{" "}
-          and{" "}
-          <span className="flex-inline items-center space-x-1">
-            <Link
-              href="https://www.peoplegrove.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              PeopleGrove
-            </Link>
-            <OpenInNew className="text-sm" />
-          </span>
-          . Check out <Link href="/about">my about page</Link> for more.
+          <Link
+            href="https://replit.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Replit
+          </Link>
+          <OpenInNew className="ml-1 text-sm" /> and{" "}
+          <Link
+            href="https://www.peoplegrove.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            PeopleGrove
+          </Link>
+          <OpenInNew className="ml-1 text-sm" />. Check out{" "}
+          <Link href="/about">my about page</Link> for more.
         </p>
         <p>
           If you need a smart generalist, who spikes in finance + go-to-market
