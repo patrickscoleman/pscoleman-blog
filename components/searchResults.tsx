8f29e909eb40dc45225536e04fbd03e2060cebb6
Patrick Coleman <patrickscoleman@gmail.com>
2024-04-12 10:57:22 -0700

    update search hint text

--- components/searchResults.tsx
+++ components/searchResults.tsx
@@ -1,13 +1,13 @@
 import { useInstantSearch, Hits } from "react-instantsearch-hooks-web";
 import { SearchResultItem } from "./searchResultItem";
 
-export const NO_RESULTS_MESSAGE = "...and returning empty-handed";
+export const NO_RESULTS_MESSAGE = "No results found...";
 
 const SearchResultsComponent = () => {
   const { indexUiState, results } = useInstantSearch();
 
   if (!indexUiState.query) {
-    return <div>Kindly awaiting your input...</div>;
+    return <div>Awaiting your input...</div>;
   } else if (indexUiState.query && results.nbHits) {
     return (
       <Hits hitComponent={SearchResultItem} classNames={{ item: "mt-0" }} />
