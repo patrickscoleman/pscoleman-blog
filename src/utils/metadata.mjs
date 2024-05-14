44f81db1b5189bf6020e4cfe4ee1cce53771c70c
Patrick Coleman <patrickscoleman@gmail.com>
2024-05-14 12:35:16 -0700

    get rid of unused preview image

--- src/utils/metadata.mjs
+++ src/utils/metadata.mjs
@@ -1,7 +1,6 @@
 export const siteMetadata = {
   title: "Patrick Coleman",
   description: "Patrick Coleman's personal site and blog",
-  previewImage: "/images/patrick.png",
   siteUrl: "https://pscoleman.me",
   firstName: "Patrick",
   lastName: "Coleman",
