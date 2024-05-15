ad28139ef73d405d1403488ebf4cd202ecec1912
Patrick Coleman <patrickscoleman@gmail.com>
2024-05-15 14:53:09 -0700

    get metadata working for first post

--- src/utils/metadata.mjs
+++ src/utils/metadata.mjs
@@ -3,6 +3,40 @@ export const siteMetadata = {
   description: "Patrick Coleman's personal site and blog",
   siteUrl: "https://pscoleman.me",
   baseUrl: "pscoleman.me",
+  twitterHandle: "@patrickscoleman",
   firstName: "Patrick",
   lastName: "Coleman",
+};
+
+export const generatePostMetadata = ({
+  title,
+  description,
+  date,
+  previewImage,
+  alt,
+}) => {
+  console.log(date);
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: `${siteMetadata.siteUrl}${previewImage}`,
+          alt,
+        },
+      ],
+      type: "article",
+      publishedTime: `${date}T00:00:00.000Z`,
+      authors: [`${siteMetadata.firstName} ${siteMetadata.lastName}`],
+    },
+    twitter: {
+      title,
+      description,
+      creator: siteMetadata.twitterHandle,
+      images: [`${siteMetadata.siteUrl}${previewImage}`],
+    },
+  };
 };
