dbb0dd42a7b3ea3afec21520700066396884db22
Patrick Coleman <patrickscoleman@gmail.com>
2024-05-15 10:51:33 -0700

    add test for getting metadata from blog posts

--- src/utils/preBuildDevAndProd.mjs
+++ src/utils/preBuildDevAndProd.mjs
@@ -1,6 +1,9 @@
 // import { getAllPosts } from "./getAllPosts.mjs";
 // import { generateRssFeed } from "./generateRssFeed.mjs";
+import { test } from "./test.mjs";
 
 // const postData = await getAllPosts();
 
 // generateRssFeed(postData);
+
+test();
