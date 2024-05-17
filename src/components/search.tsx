a11435e44b3732d975fe155c1e17c498f657ad58
Patrick Coleman <patrickscoleman@gmail.com>
2024-05-17 09:57:31 -0700

    add UI for search button

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,24 +1,46 @@
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Search as SearchIcon } from "lucide-react";
 
 export function Search() {
   return (
     <div>
-      <Button className="md:hidden" size="icon" variant="outline">
-        <SearchIcon className="h-4 w-4" />
-        <span className="sr-only">Search</span>
-      </Button>
-      <form className="hidden md:block">
-        <div className="relative">
-          <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
-          <Input
-            className="w-full bg-white shadow-none appearance-none pl-8 md:w-[200px] dark:bg-gray-950"
-            placeholder="Search..."
-            type="search"
-          />
-        </div>
-      </form>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div>
+            <Button className="md:hidden" size="icon" variant="outline">
+              <SearchIcon className="h-4 w-4" />
+              <span className="sr-only">Search</span>
+            </Button>
+            <Button
+              className="items-center justify-between hidden md:flex w-48"
+              variant="outline"
+            >
+              <span className="text-muted-foreground">Search...</span>
+              <SearchIcon className="h-4 w-4 mr-1" />
+            </Button>
+          </div>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Search</DialogTitle>
+            <DialogDescription>Search</DialogDescription>
+          </DialogHeader>
+          <p>Results</p>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
